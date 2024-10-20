66d7279ddcd6def3a0e84541e6b6f00fe0c5df56
wijdane121 <wijdanemg17@gmail.com>
2024-10-20 18:00:10 +0100

    createUtilisateurService

---
+++ src/test/java/uir/ac/ma/todolist/service/UtilisateurServiceTest.java
@@ -0,0 +1,45 @@
+package uir.ac.ma.todolist.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import uir.ac.ma.todolist.entity.Utilisateur;
+import uir.ac.ma.todolist.repository.UtilisateurRepository;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+public class UtilisateurServiceTest {
+
+    @Mock
+    private UtilisateurRepository utilisateurRepository;
+
+    @InjectMocks
+    private UtilisateurService utilisateurService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void createUtilisateur_ShouldReturnNewUtilisateur() {
+        Utilisateur utilisateur = new Utilisateur();
+        utilisateur.setNom("John Doe");
+
+        when(utilisateurRepository.save(any(Utilisateur.class))).thenReturn(utilisateur);
+
+        Utilisateur createdUtilisateur = utilisateurService.register(utilisateur);
+
+        assertNotNull(createdUtilisateur);
+        assertEquals("John Doe", createdUtilisateur.getNom());
+        verify(utilisateurRepository, times(1)).save(utilisateur);
+    }
+
+
+}
