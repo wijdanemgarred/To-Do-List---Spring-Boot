17b4086f6083cd294f58b6fd1f4306e33e919e3a
wijdane121 <wijdanemg17@gmail.com>
2024-12-12 00:24:16 +0100

    Added login

--- src/main/java/uir/ac/ma/todolist/controller/UtilisateurController.java
+++ src/main/java/uir/ac/ma/todolist/controller/UtilisateurController.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import uir.ac.ma.todolist.entity.Utilisateur;
